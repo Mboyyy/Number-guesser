c5fa8e5e9103d26de9ee9d166d5a1aa1bff55177
Mboyy789 <92560974+Mboyyy@users.noreply.github.com>
2022-03-31 23:44:42 +0700

    Add files via upload

---
+++ number_guesser.py
@@ -0,0 +1,36 @@
+from msilib.schema import Error
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('please type a number larger than 0 next time.')
+        quit()
+else:
+    print('please type a number next time.')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('please type a number next time.')
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+
+print("You got it in " + str(guesses) + " guesses")
